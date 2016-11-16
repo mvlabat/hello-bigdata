7dcb69236747d9d88c79de73a8fa1dbc0cd6e9bc
mvlabat <mvlabat@gmail.com>
2016-11-16 16:59:37 +0200

    Fix writing to DB
    Move Database case classes to upper scope (not child classes anymore)
    Utilize MongoSpark.write and set collections explicitly in order to prevent bugs with saving data (see Database.writeAndSave)

--- src/main/scala/Database.scala
+++ src/main/scala/Database.scala
@@ -1,6 +1,7 @@
 import akka.actor.Actor
+import com.mongodb.spark.MongoSpark
 import org.apache.spark.SparkConf
-import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.{DataFrame, SparkSession}
 import com.mongodb.spark.sql._
 
 case class CheckEmailDoesNotExistMessage(email: String)
@@ -8,13 +9,13 @@ case class CheckUserExistsMessage(email: String, password: String)
 case class CreateUserMessage(email: String, password: String)
 case class SetUserIdMessage(email: String, id: String)
 
+case class User(email: String, password: String)
+case class Id(email: String, id: String)
+
 /**
   * Handles mongodb sessions and returns specific model data
   */
 class Database extends Actor {
-
-  case class User(email: String, password: String)
-  case class Id(email: String, id: String)
 
   private val conf = new SparkConf()
     .setAppName("test_task")
@@ -64,20 +65,24 @@ class Database extends Actor {
 
   private def createUser(email: String, password: String) = {
     val rdd = userSession.sparkContext.parallelize(Seq(User(email, password)))
-    userSession.createDataFrame(rdd).saveToMongoDB()
+    writeAndSave(userSession.createDataFrame(rdd), "users")
   }
 
   private def setUserId(email: String, id: String) = {
-    val df = userSession.loadFromMongoDB().toDF()
+    val df = idsSession.loadFromMongoDB().toDF()
     val emailExists = df.filter(df("email").equalTo(email)).count() == 1
 
     // If email already exists, we have to override its value.
     if (emailExists) {
-      // TODO: for some reason this line truncates "users" collection
-      // MongoSpark.write(df.filter(df("email").notEqual(email))).mode("overwrite").save()
+      val filtered = df.filter(df("email").notEqual(email))
+      writeAndSave(filtered, "ids", "overwrite")
     }
 
     val rdd = idsSession.sparkContext.parallelize(Seq(Id(email, id)))
-    idsSession.createDataFrame(rdd).saveToMongoDB()
+    writeAndSave(idsSession.createDataFrame(rdd), "ids")
+  }
+
+  private def writeAndSave(dataFrame: DataFrame, collection: String, mode: String = "append") = {
+    MongoSpark.write(dataFrame).option("collection", collection).mode(mode).save()
   }
 }
